96452a5bf5b0000e613a4c30f520a16bb4c6af85
thuvi6 <57703633+thuvi6@users.noreply.github.com>
2019-11-14 16:48:15 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
-public class HelloWorld {
+public class HelloWorld
+{   
+      public static void main(String[] args)
+      {       
+         System.out.println("Hello gist @ GitHub!");   
+      }
+} 
 
-  public static void main(final String[] args) {
-    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
-    final LocalDateTime now = LocalDateTime.now();
-
-    System.out.println("Hello, World! The current time is " + dtf.format(now));
-  }
-  }
+  
   
